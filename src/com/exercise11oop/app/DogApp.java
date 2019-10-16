5d15de68fdb31f1f1191679365f0b128d5258a5c
DanielCandiaWR <padilla_da@hotmail.com>
2019-10-16 15:27:04 -0500

    Dog class created

---
+++ src/com/exercise11oop/app/DogApp.java
@@ -0,0 +1,12 @@
+package com.exercise11oop.app;
+import com.exercise11oop.model.Dog;
+
+public class DogApp {
+	public static void main(String[] args) {
+		Dog pluto = new Dog();	
+		pluto.age = 15;
+		pluto.height = 50;
+		pluto.id = 10;
+		
+	}
+}
